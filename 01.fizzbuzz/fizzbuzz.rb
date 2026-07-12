8a949b44df8d23462991847b450cb6bb812ba99e
takumi-onisi <t93onisi@gmail.com>
2026-07-12 11:04:45 +0900

    Add logic to branch the output based on value

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,4 +1,15 @@
 RANGE = 1..20
 RANGE.each do |num| 
-  puts num
+  # 特定の倍数の時は出力内容を変える
+  # 出力結果が判定の順番によって変化しないように条件で排他性を担保する
+  case
+    when num % 3 == 0 && num % 5 != 0
+      puts 'Fizz'
+    when num % 3 != 0 && num % 5 == 0
+      puts 'Buzz'
+    when num % 3 == 0 && num % 5 == 0
+      puts 'FizzBuzz'
+    else
+      puts num
+  end
 end
