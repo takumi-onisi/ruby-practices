6ce0375b98860a2e4da1828db45597f66143e36a
takumi-onisi <t93onisi@gmail.com>
2026-07-12 17:23:38 +0900

    Set year and month from ARGV or default to Date.today

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 require 'optparse'
+require 'date'
 
 options = ARGV.getopts('y:', 'm:')
-puts options['y']
-puts options['m']
+YEAR = options['y'] ? options['y'] : Date.today.year
+MONTH = options['m'] ? options['m'] : Date.today.month
+p YEAR
+p MONTH
