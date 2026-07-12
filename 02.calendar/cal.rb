f77e6143778df9b63b68aa0f5bb7055e00dd1234
takumi-onisi <t93onisi@gmail.com>
2026-07-12 17:29:53 +0900

    Add explanatory comments for year and month initialization

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,7 +2,9 @@
 require 'optparse'
 require 'date'
 
+# カレンダーで表示させる 年 と 月 を初期化
 options = ARGV.getopts('y:', 'm:')
+# 指定がない場合は今日の 年 と 月を使用する
 YEAR = options['y'] ? options['y'] : Date.today.year
 MONTH = options['m'] ? options['m'] : Date.today.month
 p YEAR
