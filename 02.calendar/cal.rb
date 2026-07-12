6d62a9efcb0b71fba122cafa991eb59c1880b1dd
takumi-onisi <t93onisi@gmail.com>
2026-07-12 19:26:23 +0900

    Instantiate Date object for the first day of the initialized month
    - コマンドライン引数から渡された値をあらかじめ整数型に変換する
    - Dateインスタンス生成時に例外を捕捉し、正しい値の入力を促す

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,7 +5,15 @@ require 'date'
 # カレンダーで表示させる 年 と 月 を初期化
 options = ARGV.getopts('y:', 'm:')
 # 指定がない場合は今日の 年 と 月を使用する
-YEAR = options['y'] ? options['y'] : Date.today.year
-MONTH = options['m'] ? options['m'] : Date.today.month
-p YEAR
-p MONTH
+YEAR = options['y'] ? options['y'].to_i : Date.today.year
+MONTH = options['m'] ? options['m'].to_i : Date.today.month
+
+# カレンダーで表示させるDateインスタンスを生成
+begin
+  first_date = Date.new(YEAR, MONTH, 1)
+rescue
+  puts 'エラー : カレンダーの生成に失敗しました'
+  puts '-yと-mのオプションを正しく渡しているか確認してください'
+end
+
+puts first_day
