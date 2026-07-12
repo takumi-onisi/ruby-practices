f63b497dded3751e24d124b159801a1eb6e51f87
takumi-onisi <t93onisi@gmail.com>
2026-07-12 15:39:33 +0900

    refactor: evaluate 15s multiple first in FizzBuzz
    判定の方法を`if/elsif/else`を用いたよりRubyらしいやり方に修正
    15の倍数を先に判定することで、後続の判定条件を簡素化

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,15 +1,13 @@
 RANGE = 1..20
 RANGE.each do |num|
   # 特定の倍数の時は出力内容を変える
-  # 出力結果が判定の順番によって変化しないように条件で排他性を担保する
-  case
-    when num % 3 == 0 && num % 5 != 0
-      puts 'Fizz'
-    when num % 3 != 0 && num % 5 == 0
-      puts 'Buzz'
-    when num % 3 == 0 && num % 5 == 0
-      puts 'FizzBuzz'
-    else
-      puts num
+  if num % 3 == 0 && num % 5 == 0 then
+    puts 'FizzBuzz'
+  elsif num % 3 == 0 then
+    puts 'Fizz'
+  elsif num % 5 == 0 then
+    puts 'Buzz'
+  else
+    puts num
   end
 end
