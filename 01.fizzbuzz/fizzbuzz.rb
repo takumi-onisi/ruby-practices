26b34c5733f96b4b533b15b23f646023fe737a6d
takumi-onisi <t93onisi@gmail.com>
2026-07-17 20:43:01 +0900

    Rename loop variable from num to n for brevity

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
-1.upto(20) do |num|
-  if num % 3 == 0 && num % 5 == 0
+1.upto(20) do |n|
+  if n % 3 == 0 && n % 5 == 0
     puts 'FizzBuzz'
-  elsif num % 3 == 0
+  elsif n % 3 == 0
     puts 'Fizz'
-  elsif num % 5 == 0
+  elsif n % 5 == 0
     puts 'Buzz'
   else
-    puts num
+    puts n
   end
 end
