7012e38335a6afab1d80bfd840573ec3a1a343be
takumi-onisi <t93onisi@gmail.com>
2026-07-17 20:32:31 +0900

    Remove unnecessary then keywords from if expressions

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,10 @@
 RANGE = 1..20
 RANGE.each do |num|
-  if num % 3 == 0 && num % 5 == 0 then
+  if num % 3 == 0 && num % 5 == 0
     puts 'FizzBuzz'
-  elsif num % 3 == 0 then
+  elsif num % 3 == 0
     puts 'Fizz'
-  elsif num % 5 == 0 then
+  elsif num % 5 == 0
     puts 'Buzz'
   else
     puts num
