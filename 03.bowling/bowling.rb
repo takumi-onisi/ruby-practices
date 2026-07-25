dca6caf274e1fe1b829d82a7002620074f97f50e
takumi-onisi <t93onisi@gmail.com>
2026-07-25 21:37:41 +0900

    Fix consecutive strike score calculation using each_cons(3)
    - Represent all strike frames as [10] instead of [10,0]
    - Use each_cons(3) to fetch the next two shots correctly

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -12,14 +12,12 @@ scores.each do |s|
   end
 end
 
-sliced_shots = shots.each_slice(2).to_a
-tenth_frame_shots = sliced_shots.drop(9).flat_map {|s| s.eql?([10,0]) ? 10 : s }
-frames = sliced_shots.take(9).push tenth_frame_shots
-
+tmp_frames = shots.each_slice(2).to_a.map {|s| s.eql?([10,0]) ? [10] : s}
+frames = tmp_frames.take(9).push tmp_frames[9..-1].flatten
 point = 0
-frames.each_cons(2) do |cons_frames|
+frames.push([0]).each_cons(3) do |cons_frames|
   current_points = cons_frames[0]
-  next_points = cons_frames[1]
+  next_points = cons_frames[1].dup.concat cons_frames[2]
   if current_points[0] == 10
     point += current_points.sum + next_points.take(2).sum
     next
