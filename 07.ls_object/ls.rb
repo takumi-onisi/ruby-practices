2a861c3b4693c5456cd88df587ffca4a778d2d5f
takumi-onisi <t93onisi@gmail.com>
2026-08-19 21:08:32 +0900

    Refactor: Replace Proc with block and optimize code structure
    - Replace Proc with yield to use block execution based on code review
    - Define main method to encapsulate top-level execution
    - Optimize variable names, method chains, and method usage

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -1,30 +1,27 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-output_col_count = 3
+OUTPUT_COL_COUNT = 3
 
-def print_vertical_columns(array, col_count, formatter)
+def main
+  file_names = Dir.children('.').sort
+  file_names = file_names.reject { |f| f[0] == '.' }
+  max_length = file_names.map(&:length).max
+  print_vertical_columns(file_names, OUTPUT_COL_COUNT) do |file_name|
+    file_name.ljust(max_length)
+  end
+end
+
+def print_vertical_columns(array, col_count)
   row_count = (array.length / col_count.to_f).ceil
-  (0...row_count).each do |row|
-    (0...col_count).each do |col|
+  row_count.times.each do |row|
+    col_count.times.each do |col|
       elem = array[row + col * row_count]
-      print formatter.call(elem) if elem
+      print yield(elem) if elem
       print ' '
     end
     puts
   end
 end
 
-def format_file_name(name, length)
-  name.ljust(length)
-end
-
-files = Dir.children('.').sort
-files = files.reject { |f| f[0] == '.' }
-
-max_length = files.max do |a, b|
-  a.length <=> b.length
-end.length
-formatter = proc { |name, length = max_length| format_file_name(name, length) }
-
-print_vertical_columns(files, output_col_count, formatter)
+main
