bb22a60102455ef3b3ba035c85e0a8d55e2a816f
takumi-onisi <t93onisi@gmail.com>
2026-09-08 21:06:33 +0900

    Rename file-related variables to entry-based naming

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -9,10 +9,10 @@ def main
   command_options = []
   opt.on('-a') { command_options.push :a }
   opt.parse(ARGV)
-  file_names = list_entries(command_options)
-  file_names = file_names.sort
-  max_length = file_names.map(&:length).max
-  print_vertical_columns(file_names, OUTPUT_COL_COUNT) do |file_name|
+  entry_names = list_entries(command_options)
+  entry_names = entry_names.sort
+  max_length = entry_names.map(&:length).max
+  print_vertical_columns(entry_names, OUTPUT_COL_COUNT) do |file_name|
     file_name.ljust(max_length)
   end
 end
@@ -26,12 +26,12 @@ def list_entries(command_options)
   end
 end
 
-def print_vertical_columns(file_names, col_count)
-  row_count = (file_names.length / col_count.to_f).ceil
+def print_vertical_columns(entry_names, col_count)
+  row_count = (entry_names.length / col_count.to_f).ceil
   row_count.times.each do |row|
     col_count.times.each do |col|
-      elem = file_names[row + col * row_count]
-      print yield(elem) if elem
+      entry_name = entry_names[row + col * row_count]
+      print yield(entry_name) if entry_name
       print ' '
     end
     puts
